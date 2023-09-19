6a49c32883ab07ef5c86b1163423ba5c8de5e4e8
Li Zhe Yun <93893933+WindowsCommandPrompt@users.noreply.github.com>
2023-09-19 12:56:26 +0800

    Update config.js
    Completed configuration files for AJAX requests

--- config/config.js
+++ config/config.js
@@ -1,7 +1,58 @@
 export const getRequest = Object.freeze({
-  
+  "async": true,
+  "crossDomain": true,
+  "url": "https://newdatabase1-1d27.restdb.io/rest/logininformation",
+  "method": "GET",
+  "headers": {
+    "content-type": "application/json",
+    "x-apikey": "61d254f5ccd0211b32089499",
+    "cache-control": "no-cache"
+  }
 })
 
-export const postRequest = Object.freeze({ 
-}); 
+export const postRequest = (data={ }) => { 
+  return {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://newdatabase1-1d27.restdb.io/rest/logininformation",
+    "method": "POST",
+    "headers": {
+      "content-type": "application/json",
+      "x-apikey": "61d254f5ccd0211b32089499",
+      "cache-control": "no-cache"
+    },
+    "processData": false,
+    "data": JSON.stringify(data)
+  }; 
+};
 
+export const DeleteRequest = (key) => {
+  return { 
+      "async": true,
+      "crossDomain": true,
+      "url": `https://newdatabase1-1d27.restdb.io/rest/logininformation/${key}`,
+      "method": "DELETE",
+      "headers": {
+        "content-type": "application/json",
+        "x-apikey": "61d254f5ccd0211b32089499",
+        "cache-control": "no-cache"
+      },
+      "processData": false
+  }; 
+} 
+
+export const PutRequestConfig = (key, data={ }) => {
+  return { 
+    "async": true,
+    "crossDomain": true,
+    "url": `https://newdatabase1-1d27.restdb.io/rest/logininformation/${key}`,  //Take in the key from the function parameters
+    "method": "PUT",
+    "headers": {
+      "content-type": "application/json",
+      "x-apikey": "61d254f5ccd0211b32089499",
+      "cache-control": "no-cache"
+    },
+    "processData": false,
+    "data": JSON.stringify(data)  //Take in data from the second parameter 
+  }; 
+} 
